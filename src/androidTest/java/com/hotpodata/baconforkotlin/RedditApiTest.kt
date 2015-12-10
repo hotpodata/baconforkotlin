8071b173c3f6e574d2c1db722b87c7d33046ea13
Joel Drotos <jrdrotos@gmail.com>
2015-12-10 10:12:09 -0600

    Initial commit, working userless client for reddit api

---
+++ src/androidTest/java/com/hotpodata/baconforkotlin/RedditApiTest.kt
@@ -0,0 +1,12 @@
+package com.hotpodata.baconforkotlin
+
+import android.test.AndroidTestCase
+
+/**
+ * Created by jdrotos on 11/29/15.
+ */
+class RedditApiTest : AndroidTestCase() {
+    override fun setUp() {
+        super.setUp()
+    }
+}
---
+++ src/main/java/com/hotpodata/baconforkotlin/network/model/t1.kt
@@ -0,0 +1,12 @@
+package com.hotpodata.baconforkotlin.network.model
+
+/**
+ * Created by jdrotos on 11/29/15.
+ */
+class t1 : ThingData {
+    var subreddit: String? = null
+    var ups: Int? = null
+    var body: String? = null
+    var body_html: String? = null
+    var replies: Thing? = null
+}
